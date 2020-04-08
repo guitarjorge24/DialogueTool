01b6c448d903d344128c47b7dfcb5cae01171ba8
Jorge <guitarjorge24@outlook.com>
2020-04-08 09:31:22 -0400

    First Commit, before implementing custom editor.

---
+++ Assets/Scripts/Dialogue.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Dialogue 1", menuName = "Dialogue", order = 1)]
+
+public class Dialogue : ScriptableObject
+{
+	public string[] CharactersList;
+	//DialogueElement.Characters;
+
+	public List<DialogueElement> DialogueItems;
+}
+
---
+++ Assets/Scripts/DialogueElement.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+[System.Serializable] //needed to make ScriptableObject out of this class
+public class DialogueElement
+{
+	public enum Characters { CharacterA, CharacterB};
+	public enum AvatarPos { left, right };
+
+	[Header("CHARACTER")]
+	public Characters Character;
+	public AvatarPos CharacterPosition;
+	public Texture2D CharacterPic;
+
+	[Header("DIALOGUE")]
+	[Tooltip("What the character is saying")]
+	public string DialogueText;
+	public GUIStyle DialogueTextStyle;
+	[Tooltip("How many letters per second")]
+	public float TextPlayBackSpeed;
+
+	public AudioClip PlayBackSoundFile;
+}
