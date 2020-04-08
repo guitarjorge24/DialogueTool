d147479544274d3841fab064ac0bc9fee1e7cb20
Jorge <guitarjorge24@outlook.com>
2020-04-08 15:15:37 -0400

    Added DialogController to output dialog on screen letter by letter. Added Text Mesh Pro to project, a "next line" button, and a text box to display.

---
+++ Assets/Scripts/DialogueController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+
+public class DialogueController : MonoBehaviour
+{
+    [Tooltip("Select or drag the Dialog.asset for this level here")]
+    public Dialogue LoadedDialogue;
+    public TextMeshProUGUI textDisplay;
+    public float typingSpeed;
+
+    private int index = 0;
+
+    void Start()
+    {
+        StartCoroutine(Type());
+    }
+    IEnumerator Type()
+    {
+        foreach (char letter in LoadedDialogue.DialogueItems[index].DialogueText)
+        {
+            textDisplay.text += letter;
+            yield return new WaitForSeconds(typingSpeed);
+        }
+    }
+
+    public void NextSentence()
+    {
+
+        if (index < LoadedDialogue.DialogueItems.Count - 1)
+        {
+            index++;
+            textDisplay.text = "";
+            StartCoroutine(Type());
+        }
+        else
+        {
+            textDisplay.text = "";
+        }
+    }
+
+
+}
